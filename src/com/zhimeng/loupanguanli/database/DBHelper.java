9c9035a50bf7178f69ca5f50bc97b4a915972c62
ivey-in <ivey_in@163.com>
2014-07-21 20:56:00 +0800

    second commit

---
+++ src/com/zhimeng/loupanguanli/database/DBHelper.java
@@ -0,0 +1,26 @@
+package com.zhimeng.loupanguanli.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+	private static final String DB_NAME = "loupan.db";
+	private static final int VERSION = 1;
+
+	public DBHelper(Context context) {
+		super(context, DB_NAME, null, VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+	}
+
+}
